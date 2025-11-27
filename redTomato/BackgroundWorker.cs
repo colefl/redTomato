641187118ef4dda0d5a71a734c350b6f37f49290
Cole <cflick@iastate.edu>
2025-11-27 01:03:56 -0600

    Start of timer extension

---
+++ redTomato/BackgroundWorker.cs
@@ -0,0 +1,20 @@
+﻿using Blazor.BrowserExtension;
+
+namespace redTomato
+{
+    public partial class BackgroundWorker : BackgroundWorkerBase
+    {
+        [BackgroundWorkerMain]
+        public override void Main()
+            => WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+
+        async Task OnInstalled()
+        {
+            var indexPageUrl = WebExtensions.Runtime.GetURL("index.html");
+            await WebExtensions.Tabs.Create(new()
+            {
+                Url = indexPageUrl
+            });
+        }
+    }
+}
